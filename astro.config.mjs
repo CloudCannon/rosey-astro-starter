bb4a51af7f6a19f17a27ed28489ad2209b2e298a
tomrcc <tom@cloudcannon.com>
2025-03-22 11:15:29 +1300

    Update astro and CC config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import bookshop from "@bookshop/astro-bookshop";
 import tailwind from "@astrojs/tailwind";
 import alpine from "@astrojs/alpinejs";
 import mdx from "@astrojs/mdx";
-import { autoAddRoseyTags } from "./src/autoAddRoseyTags";
+import { autoAddRoseyTags } from "./rosey/utils/ssgs/astroMarkdownPlugin";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +12,18 @@ export default defineConfig({
   integrations: [react(), tailwind(), bookshop(), alpine(), mdx()],
   markdown: {
     rehypePlugins: [autoAddRoseyTags],
+    remarkRehype: {
+      // https://github.com/syntax-tree/mdast-util-to-hast?tab=readme-ov-file#options
+      handlers: {
+        mdxJsxTextElement(state, node) {
+          return {
+            type: "element",
+            tagName: node.name,
+            properties: {},
+            children: state.all(node),
+          };
+        },
+      },
+    },
   },
 });
