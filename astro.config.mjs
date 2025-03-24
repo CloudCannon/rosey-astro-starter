de329b4ec9be06964d9a3a047edc8bfefe45208d
tomrcc <tom@cloudcannon.com>
2025-03-25 10:14:38 +1300

    Update paths in prep for site mounting rosey-connector

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import bookshop from "@bookshop/astro-bookshop";
 import tailwind from "@astrojs/tailwind";
 import alpine from "@astrojs/alpinejs";
 import mdx from "@astrojs/mdx";
-import { autoAddRoseyTags } from "./rosey-connector/ssgs/astroMarkdownPlugin.ts";
+import { autoAddRoseyTags } from "./rosey-connector/ssgs/astroMarkdownTaggerPlugin.ts";
 
 // https://astro.build/config
 export default defineConfig({
