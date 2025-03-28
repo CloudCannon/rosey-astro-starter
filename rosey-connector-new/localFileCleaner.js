97a148d2ccef557c3f0da730f55c9cb2ab5069c2
tomrcc <tom@cloudcannon.com>
2025-03-28 13:34:25 +1300

    Update rosey-connector with upstream changes

--- rosey-connector-new/localFileCleaner.js
+++
@@ -1,9 +0,0 @@
-import { checkLocales } from "./checkLocales.js";
-import { readConfigFile } from "./helpers/file-helper.js";
-
-(async () => {
-  const configData = await readConfigFile("./rosey/config.yaml");
-  console.log("🏗️🏗️ Checking locales...");
-  await checkLocales(configData);
-  console.log("🏗️🏗️ Checked locales...");
-})();
